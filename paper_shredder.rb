a818930ffe5fd028306687a13f5668530520a82c
RyanNovas <ryan@quill.org>
2016-08-24 19:37:23 -0400

    adds help command, breaks up method
    adds help command, breaks up method

--- paper_shredder.rb
+++ paper_shredder.rb
@@ -4,34 +4,43 @@ require 'sinatra/reloader'
 require 'json'
 require 'httparty'
 
-
-
 URL = "https://slack.com/api/im.history?token=#{ENV['SLACK_API_KEY']}&channel=".freeze
 DELETE_URL = "https://slack.com/api/chat.delete?token=#{ENV['SLACK_API_KEY']}&ts=".freeze
-get '/' do
+
+def delete_last_number(num_to_delete, params)
+    user_id = params.fetch('user_id')
+    channel_id = params.fetch('channel_id')
+    channel_name = params.fetch('channel_name')
+    user_history = HTTParty.get(URL + channel_id)
+    messages = user_history['messages']
+    user_messages = []
+    count = 0
+    messages.each do |message|
+        if message['user'] == user_id
+            HTTParty.get(DELETE_URL + message['ts'] + '&channel=' + channel_id)
+            count += 1
+        end
+        break if count == num_to_delete
+    end
 end
 
 post '/' do
     text = params.fetch('text').strip.split(' ')
-    user_id = params.fetch('user_id')
-    channel_id = params.fetch('channel_id')
     num_to_delete = text[1].to_i
-    channel_name = params.fetch('channel_name')
-    if text.first == 'last' && num_to_delete
-        if channel_name == 'directmessage'
-            user_history = HTTParty.get(URL + channel_id)
-            messages = user_history['messages']
-            user_messages = []
-            count = 0
-            messages.each do |message|
-                if message["user"] == user_id
-                  HTTParty.get(DELETE_URL + message["ts"] + '&channel=' + channel_id)
-                  count += 1
-                end
-                break if count == num_to_delete
-            end
-        end
 
+    if text.first == 'help'
+        message = "You can: \n
+                   - bulk delete direct messages by typing '/paper_shredder last (#of messages to delete)' \n
+                   - view the source code at https://github.com/RyanNovas/paper_shredder \n
+                   - learn more about the creator at www.ryannovas.com
+                  "
+    elsif 'directmessage' != params.fetch('channel_name')
+        message = 'You can only your own delete direct messages. Type /paper_shredder help to learn more.'
+    elsif text.first == 'last' && num_to_delete
+        delete_last_number(num_to_delete, params)
+        message = "What last #{num_to_delete} messages?"
+    else
+        message = 'That is an invalid command. Type /paper_shredder help to learn more.'
     end
-    params.fetch('text').split(' ').to_s
+    message
 end
